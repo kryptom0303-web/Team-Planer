7967347c35209edec0da6d88d00e76bd6c519905
kryptom0303-web <kryptom0303@gmail.com>
2026-05-04 10:06:59 +0200

    Update App.jsx
    Unterstrich

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANONKEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function TeamPlanerApp() {
   const [events, setEvents] = useState([]);
